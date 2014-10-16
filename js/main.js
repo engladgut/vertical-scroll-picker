c604f8cb631545b0b5c05ed58249152e1ba8a3bf
LeeHyunwoo <leehyunwoo@LeeHyunwooui-iMac.local>
2014-10-16 18:42:10 +0900

    remove console.log()

--- js/main.js
+++ js/main.js
@@ -7,11 +7,6 @@ $ (window ).load( function()
     } );
 	
 	
-	$( '.contents' ).on( 'click', function()
-	{
-//		$( '#textbox' ).html( $( '#textbox' ).html() == 'Basic' ? 'Sample' : 'Basic' );				
-	} );
-	
 	$( '.contents > div' ).each( function()
 	{
 		var mc = new Hammer( this ),
@@ -24,9 +19,6 @@ $ (window ).load( function()
 				posY = ev.deltaY + lastPosY,
 				v = ev.velocityY;
 			
-//			console.log( $self.attr( 'id' ), 'lastPosY = ' + lastPosY );
-//			console.log( $self.attr( 'id' ), 'posY = ' + posY );
-			
 			$self.find( 'ul' ).css( 'transform', 'translate3d( 0, ' + posY + 'px, 0 )' );
 			$self.data( 'velocity-y', v );
 		} );
@@ -37,8 +29,6 @@ $ (window ).load( function()
 			var lastPosY = Number( $self.data( 'position-y' ) ),
 				posY = ev.deltaY + lastPosY,
 				v = Math.abs( $self.data( 'velocity-y' ) );
-			
-//			console.log( 'v', v );
 			
 			if( v > 4 )
 				v = 4;
@@ -54,19 +44,11 @@ $ (window ).load( function()
 					d *= .5;
 				}
 				
-//				console.log( 'ev.deltaY', ev.deltaY );
-//				console.log( 'posOffsetY', posOffsetY );
-				
 				posY += posOffsetY;
 			}
 			
 			var itemHeight = 100,
 				overflow = itemHeight - Math.abs( posY % 100 );
-			
-//			console.log( '====================================' );
-//			console.log( 'posY before', posY );
-//			console.log( 'posY % 100', ( posY % 100 ) );
-//			console.log( 'overflow', overflow );
 			
 			if( Math.abs( posY % 100 ) < 50 ) posY -= posY % 100;
 			else
@@ -74,8 +56,6 @@ $ (window ).load( function()
 				if( posY < 0 ) posY -= overflow;
 				else posY += overflow;
 			}
-			
-//			console.log( 'posY after', posY );
 			
 			$self.find( 'ul' ).css( 'transform', 'translate3d( 0, ' + posY + 'px, 0 )' );
 			$self.data( 'position-y', posY );
